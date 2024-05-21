d6ae2778a01d614a94cb3ad9a2785bf9a3b74c62
Abhisar Anand <abhisar.muz@gmail.com>
2024-05-20 23:07:17 -0400

    removed comments

--- server.py
+++ server.py
@@ -4,23 +4,19 @@ from flask import Flask, render_template, Response, jsonify
 
 app = Flask(__name__)
 
-# Global variable to store information
 server_info = {
     "fps": 0,
     "resolution": (0, 0)
 }
 
-# Initialize the MediaPipe Pose model
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
 pose = mp_pose.Pose()
 
 def pose_detection(frame):
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # Run the MediaPipe Pose model on the image
     results = pose.process(image)
 
-    # Draw the pose landmarks on the image
     if results.pose_landmarks:
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
